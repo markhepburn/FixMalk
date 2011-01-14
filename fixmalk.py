6fa6d6fad6bcd48e828cde6bb08fe468de131324
Mark Hepburn <Mark.Hepburn@gmail.com>
2011-01-14 23:31:59 +1100

    Add function to correct xml (add the appropriate rel attribute)

--- fixmalk.py
+++ fixmalk.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python2.5
 
-import web
+import cStringIO as StringIO
 import logging
+import web
+import xml.etree.ElementTree as et
 
 routes = (
     '/(.*)', 'Handler'
 )
 
 remote_url = 'http://www.drinkmalk.com/books/'
+nslink = '{http://www.w3.org/2005/Atom}link'
+relaccess = 'http://opds-spec.org/acquisition/open-access'
+linktype = 'application/epub' # +zip, sometimes
 
 class Handler(object):
     def GET(self, req):
         logging.debug('got request "%s"' % req)
         return 'ping -> pong'
+
+def fixLinks(xml):
+    tree = et.parse(xml)
+    for link in tree.getiterator(nslink):
+        if link.get('type').startswith(linktype):
+            link.set('rel', relaccess)
+    outputstr = StringIO()
+    tree.write(outputstr)
+    return outputstr.getvalue()
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
